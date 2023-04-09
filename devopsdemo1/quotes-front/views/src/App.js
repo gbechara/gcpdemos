2fa1954e3a48533280ad0c8b192f1299b55f9b51
gbechara <mail@example.com>
2023-04-09 17:08:39 +0000

    refactor react code and use react env var

--- devopsdemo1/quotes-front/views/src/App.js
+++ devopsdemo1/quotes-front/views/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import QuotesList from './js/quoteslist';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
+ /*   <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,8 +20,18 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </div>*/
+    <Router>
+      <div className="main">
+        <div>
+          <QuotesList />
+        </div>
+      </div>
+      </Router>
   );
 }
+
+//ReactDOM.render(<App />, document.getElementById('monapp'));
+
 
 export default App;
--- devopsdemo1/quotes-front/views/src/index.js
+++ devopsdemo1/quotes-front/views/src/index.js
@@ -8,12 +8,13 @@ import reportWebVitals from './reportWebVitals';
   console.log('webpack worked')
 })*/
 
-/*const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('monapp'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);*/
+);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
