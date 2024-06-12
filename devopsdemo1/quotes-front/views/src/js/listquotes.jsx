431ee36e6fe01cebcfa9b6d7c66946b65e6432dd
gbechara <mail@example.com>
2024-06-12 09:57:59 +0000

    demo lab

--- devopsdemo1/quotes-front/views/src/js/listquotes.jsx
+++ devopsdemo1/quotes-front/views/src/js/listquotes.jsx
@@ -121,7 +121,7 @@ class LoggedIn extends React.Component {
                 <a onClick={this.logout}>Log out</a>
               </div>*/}
 
-              <h3>List of quotes and writers v0.7.8</h3>
+              <h3>List of quotes and writers v0.7.9</h3>
               <div className="row">
                 {this.state.citations.map(function(citation, i){
                   return (<Citation key={i} citation={citation} />);
