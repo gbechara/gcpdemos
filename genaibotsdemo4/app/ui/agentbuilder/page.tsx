3ae57048a31bb489e2cd2ed4965cdae484d6ebbf
gbechara <mail@example.com>
2026-02-09 08:38:52 +0000

    node version for workstation

--- genaibotsdemo4/app/ui/agentbuilder/page.tsx
+++ genaibotsdemo4/app/ui/agentbuilder/page.tsx
@@ -1,6 +1,7 @@
 import { ChatWindow } from "@/components/ChatWindow";
 import { lusitana } from '@/app/ui/fonts';
 import {Cat} from 'lucide-react'
+import Script from 'next/script'
 
 export default function Home() {
   const InfoCard = (
@@ -19,6 +20,17 @@ export default function Home() {
       placeholder="I'm an Agent created by agent builder on Vertex AI!"
       emptyStateComponent={InfoCard}
     ></ChatWindow>
+    <Script src="https://www.gstatic.com/dialogflow-console/fast/df-messenger/prod/v1/df-messenger.js"></Script>
+    <df-messenger align="right" 
+      location="us-central1"
+      project-id="gab-devops-1"
+      agent-id="f4b4d341-f2c5-41fc-8167-767195ace7c1"
+      language-code="en"
+      max-query-length="-1">
+      <df-messenger-chat-bubble anchor="top-right"
+      chat-title="Dialogflow Messenger">
+      </df-messenger-chat-bubble>
+    </df-messenger> 
       </div>
 
   );
